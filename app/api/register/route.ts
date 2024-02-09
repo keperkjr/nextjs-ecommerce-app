e84073ed37c905f6682fda996394ab2f3fb3047b
keperkjr <keperkjr@yahoo.com>
2024-02-08 19:36:18 -0800

    Created register api route

---
+++ app/api/register/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+import dbConnect from '@/utils/dbConnect'
+import User from '@/models/user'
+import bcrypt from 'bcrypt'
+
+export async function POST(request: NextRequest) {
+    await dbConnect();
+    const body = await request.json();
+
+    try {
+        const user = await new User({
+            name: body.name,
+            email: body.email,
+            password: await bcrypt.hash(body.password, 10)
+        }).save();
+
+        console.log('user created', user);
+        return NextResponse.json({
+            status: 'success',
+            user
+        });
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+            status: 'error',
+            error
+        }, 
+        {
+            status: 500
+        });
+    }
+}
