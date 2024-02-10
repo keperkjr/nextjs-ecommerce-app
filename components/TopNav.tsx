cd9364711050238f263ae118a753e0596d28ac63
keperkjr <keperkjr@yahoo.com>
2024-02-09 22:49:09 -0800

    Logout the loggedin user

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -6,8 +6,10 @@ import { useSession, signOut } from 'next-auth/react'
 
 function TopNav() {
     const { data, status } = useSession();
-
-    console.table({ data, status });
+    
+    function logout() {
+        signOut({ callbackUrl: "/login"})
+    }
 
     return (
         <nav className="nav shadow p-2 justify-content-between mb-3">
@@ -16,12 +18,29 @@ function TopNav() {
             </Link>
             
             <div className="d-flex">
-                <Link href="/login" className="nav-link">
-                    Login
-                </Link>   
-                <Link href="/register" className="nav-link">
-                    Register
-                </Link>                              
+                {status == 'authenticated' ? 
+                (
+                    <>
+                        <Link href="/dashboard/user" className="nav-link">
+                            {data?.user?.name} Dashboard
+                        </Link>                     
+                        <a className="nav-link pointer" onClick={logout}>
+                            Logout
+                        </a>                     
+                    </>
+                ) 
+                : 
+                (
+                    <>
+                        <Link href="/login" className="nav-link">
+                            Login
+                        </Link>   
+                        <Link href="/register" className="nav-link">
+                            Register
+                        </Link>                         
+                    </>
+                )}
+                           
             </div>
         </nav>
     )
