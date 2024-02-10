4e5fe6c61b95d59d711aeee6ea8739af03bbc2ec
keperkjr <keperkjr@yahoo.com>
2024-02-10 14:54:14 -0800

    Update css

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function LoadingPage() {
+	return (
+		<div className="row text-center justify-content-center align-items-center vh-100 text-danger">
+            <div className="col">
+			    404 - Are you lost?
+            </div>
+		</div>
+	)
+}
